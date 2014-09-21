a56b70e59ca4c560fbaacbfc6dfed7573be1ceb6
Meyyappan Nachiappan <mnachiappan1995@gmail.com>
2014-09-21 02:11:07 -0700

    trying to make chart

--- public/javascripts/product.js
+++ public/javascripts/product.js
@@ -11,7 +11,8 @@ function($http, $scope, DBservice, $location){
 	$scope.categories = ['Movies', 'Books', 'Restaurants', 'Games'];
   	$scope.showAutoComplete = true;
   	$scope.showResult = false;
-
+  	var ctx = document.getElementById("myChart").getContext("2d");
+  	var myNewChart = new Chart(ctx)
 
 
 
@@ -32,6 +33,47 @@ function($http, $scope, DBservice, $location){
 	$scope.searchProduct = function(searchTerm, categoryChosen){
 		DBservice.getRatings(searchTerm);
 		$scope.results = DBservice.ratings;
+		while($scope.results == undefined || $scope.results == null || $scope.results==[]){
+			console.log('still empty');
+		}
+		
+		console.log("ratings in searchProduct: " + $scope.results);
+		
+
+		
+		var data = [
+		    {
+		        value: 300,
+		        color:"#F7464A",
+		        highlight: "#FF5A5E",
+		        label: "1 Star"
+		    },
+		    {
+		        value: 50,
+		        color: "#46BFBD",
+		        highlight: "#5AD3D1",
+		        label: "2 Stars"
+		    },
+		    {
+		        value: 100,
+		        color: "#FDB45C",
+		        highlight: "#FFC870",
+		        label: "3 Stars"
+		    },
+		    {
+		        value: 40,
+		        color: "#949FB1",
+		        highlight: "#A8B3C5",
+		        label: "4 Stars"
+		    },
+		    {
+		        value: 120,
+		        color: "#4D5360",
+		        highlight: "#616774",
+		        label: "5 Stars"
+		    }
+
+		];
 		$scope.showResult = true;
 	}
 
@@ -48,6 +90,8 @@ function($http, $scope, DBservice, $location){
 	}
 
 	$scope.goToProduct = function(name, category){
+		console.log("inside goToProduct");
 		$location.path('addRating/' + name + '/' + category);
+
 	}
 }]);
